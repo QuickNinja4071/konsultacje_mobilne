c9d0757ac5f65d635ec56af501aefafe5e3eccd7
QuickNinja4071 <szymont4071@gmail.com>
2022-04-04 18:08:37 +0200

    udany test 1

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main
-
+        setContentView(R.layout.activity_main)
+        var tekst = "okej"
+        findViewById<Button>(R.id.przycisk_1).setOnClickListener {
+            tekst = findViewById<TextInputEditText>(R.id.wczytywanie).text.toString()   // wczytywanie tekstu
+            findViewById<Button>(R.id.przycisk_1).text = tekst                          // wyświetlanie tekstu
+        }
     }
 }
